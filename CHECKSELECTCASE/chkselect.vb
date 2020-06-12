acc981e7ce9be9a81dfe8f9ff3e25a5f5448d1eb
sugandhsec <sugandhsec112@gmail.com>
2020-06-12 18:19:17 +0530

    IF ELSE

---
+++ CHECKSELECTCASE/chkselect.vb
@@ -0,0 +1,23 @@
+﻿Public Class chkselect
+
+    Private Sub chkselect_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim intinput As Integer
+        MsgBox("ENTER AN NUMBER")
+        intinput = InputBox("ENTER SOME TEXT")
+        Select Case intinput
+            Case 1
+                MsgBox("NUMBER 1 IS TYPED")
+            Case 2
+                MsgBox("NUMBER 2 IS TYPED")
+            Case 3
+                MsgBox("NUMBER 3 IS TYPED")
+            Case 4
+                MsgBox("NUMBER 4 IS TYPED")
+            Case 5
+                MsgBox("NUMBER 5 IS TYPED")
+            Case Else
+                MsgBox("NOT ANUMBER IN LIST")
+
+        End Select
+    End Sub
+End Class
---
+++ CheckIfElseillustration/elseif.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim intinput As Integer
+        MsgBox("ENETR AN INTEGER")
+        Dim str As String
+        str = InputBox("Enter Some TEXT")
+        intinput = Val(str)
+        If intinput = 1 Then
+            MsgBox("THIS IS FOR NO 1")
+        ElseIf intinput = 2 Then
+            MsgBox("THIS IS FOR NO 2")
+        Else
+            MsgBox("ESLE STATEMENT EXECUTED")
+        End If
+    End Sub
+End Class
